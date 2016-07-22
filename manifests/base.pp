218b594e8bfecf08269bc48a6b63677ca16d7867
Yury Frolov <yurafrol@mail.ru>
2016-07-22 10:42:08 +0300

    Added sysstat package and service to base profile

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,2 +1,13 @@
 class profile::base {
+	# System statistics for everyone
+	
+	package { "sysstat":
+		ensure	=> "installed",
+	}
+	
+	service { "sysstat":
+		ensure	=> running,
+		enable	=> true,
+		require	=> Package["sysstat"]
+	}  
 }
