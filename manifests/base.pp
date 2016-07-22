6e5d52ccfe4589a9564b66b00266283dc2e7eb3e
Yury Frolov <yurafrol@mail.ru>
2016-07-22 14:09:16 +0300

    Added snmp class to base profile

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,7 @@
 class profile::base {
+	$snmp_community = hiera(profile::base::snmp_community)
+        $snmp_network = hiera(profile::base::snmp_network)	
+
 	# System statistics for everyone
 	
 	package { "sysstat":
@@ -9,5 +12,10 @@ class profile::base {
 		ensure	=> running,
 		enable	=> true,
 		require	=> Package["sysstat"]
+	}
+
+	class { "snmp":
+		ro_community	=> $snmp_community,
+		ro_network	=> $snmp_network,
 	}  
 }
